738c3905421a9c8779e93afb1e94c302f2f83f95
Hans Kristian <hanskristianom@gmail.com>
2021-05-19 00:17:35 +0200

    Some of the models completed

---
+++ src/main/java/no/MCH/model/CustomerModel.java
@@ -0,0 +1,135 @@
+package no.MCH.model;
+
+public class CustomerModel {
+	private int customerNumber;
+	private String customerName;
+	private String contactFirstName;
+	private String phone;
+	private String addressLine1;
+	private String addressLine2;
+	private String city;
+	private String state;
+	private String postalCode;
+	private String country;
+	private EmployeeModel salesRepEmployee;
+	private Double creditLimit;
+	
+	public CustomerModel(Integer customerNumber) {
+		this.customerNumber = customerNumber;
+	}
+	
+	public CustomerModel(int customerNumber, String customerName, String contactFirstName, String phone,
+			String addressLine1, String addressLine2, String city, String state, String postalCode, String country,
+			EmployeeModel salesRepEmployee, Double creditLimit) {
+		super();
+		this.customerNumber = customerNumber;
+		this.customerName = customerName;
+		this.contactFirstName = contactFirstName;
+		this.phone = phone;
+		this.addressLine1 = addressLine1;
+		this.addressLine2 = addressLine2;
+		this.city = city;
+		this.state = state;
+		this.postalCode = postalCode;
+		this.country = country;
+		this.salesRepEmployee = salesRepEmployee;
+		this.creditLimit = creditLimit;
+	}
+	
+	public int getCustomerNumber() {
+		return customerNumber;
+	}
+	
+	public void setCustomerNumber(int customerNumber) {
+		this.customerNumber = customerNumber;
+	}
+	
+	public String getCustomerName() {
+		return customerName;
+	}
+	
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+	
+	public String getContactFirstName() {
+		return contactFirstName;
+	}
+	
+	public void setContactFirstName(String contactFirstName) {
+		this.contactFirstName = contactFirstName;
+	}
+	
+	public String getPhone() {
+		return phone;
+	}
+	
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+	public String getAddressLine1() {
+		return addressLine1;
+	}
+	
+	public void setAddressLine1(String addressLine1) {
+		this.addressLine1 = addressLine1;
+	}
+	
+	public String getAddressLine2() {
+		return addressLine2;
+	}
+	
+	public void setAddressLine2(String addressLine2) {
+		this.addressLine2 = addressLine2;
+	}
+	
+	public String getCity() {
+		return city;
+	}
+	
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+	public String getState() {
+		return state;
+	}
+	
+	public void setState(String state) {
+		this.state = state;
+	}
+	
+	public String getPostalCode() {
+		return postalCode;
+	}
+	
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+	
+	public String getCountry() {
+		return country;
+	}
+	
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	
+	public EmployeeModel getSalesRepEmployee() {
+		return salesRepEmployee;
+	}
+	
+	public void setSalesRepEmployee(EmployeeModel salesRepEmployee) {
+		this.salesRepEmployee = salesRepEmployee;
+	}
+	
+	public Double getCreditLimit() {
+		return creditLimit;
+	}
+	
+	public void setCreditLimit(Double creditLimit) {
+		this.creditLimit = creditLimit;
+	}
+	
+}
---
+++ src/main/java/no/MCH/model/EmployeeModel.java
@@ -0,0 +1,94 @@
+package no.MCH.model;
+
+public class EmployeeModel {
+	private Integer employeeNumber;
+	private String firstName;
+	private String lastName;
+	private String extension;
+	private String email;
+	private OfficeModel office;
+	private EmployeeModel reportsTo;
+	private String jobTitle;
+	
+	public EmployeeModel(Integer employeeNumber) {
+		this.employeeNumber = employeeNumber;
+	}
+	
+	public EmployeeModel(Integer employeeNumber, String firstName, String lastName, String extension, String email,
+			OfficeModel office, EmployeeModel reportsTo, String jobTitle) {
+		super();
+		this.employeeNumber = employeeNumber;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.extension = extension;
+		this.email = email;
+		this.office = office;
+		this.reportsTo = reportsTo;
+		this.jobTitle = jobTitle;
+	}
+	
+	public Integer getEmployeeNumber() {
+		return employeeNumber;
+	}
+
+	public void setEmployeeNumber(Integer employeeNumber) {
+		this.employeeNumber = employeeNumber;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getExtension() {
+		return extension;
+	}
+
+	public void setExtension(String extension) {
+		this.extension = extension;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public OfficeModel getOffice() {
+		return office;
+	}
+
+	public void setOffice(OfficeModel office) {
+		this.office = office;
+	}
+
+	public EmployeeModel getReportsTo() {
+		return reportsTo;
+	}
+
+	public void setReportsTo(EmployeeModel reportsTo) {
+		this.reportsTo = reportsTo;
+	}
+
+	public String getJobTitle() {
+		return jobTitle;
+	}
+
+	public void setJobTitle(String jobTitle) {
+		this.jobTitle = jobTitle;
+	}
+
+}
