cc425e7f85d26e814d8c0eae6455ede12517810b
Hans Kristian <hanskristianom@gmail.com>
2021-05-20 09:45:00 +0200

    Added MainContent

---
+++ src/main/java/no/MCH/view/MainContent.java
@@ -0,0 +1,12 @@
+package no.MCH.view;
+
+import javax.swing.JPanel;
+
+public class MainContent extends JPanel {
+	private static final long serialVersionUID = 1L;
+	
+	protected MainContent() {
+		DefaultGui.componentModel.addComponent(this);
+	}
+
+}
